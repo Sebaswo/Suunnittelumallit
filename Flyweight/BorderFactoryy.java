0a045498e317fe7f388ea65055ef2941ea40803e
Sebaswo <sebastwo@metropolia.fi>
2023-04-11 22:48:38 +0300

    Flyweight

---
+++ Flyweight/BorderFactoryy.java
@@ -0,0 +1,23 @@
+package Flyweight;
+
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderStroke;
+import javafx.scene.layout.BorderStrokeStyle;
+import javafx.scene.layout.BorderWidths;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.paint.Color;
+
+public enum BorderFactoryy {
+
+	INSTANCE;
+	
+	private Border shareableRedBorder = new Border( new BorderStroke(Color.RED, 
+            BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)); 
+	
+	
+	public Border getBorder() {
+		return shareableRedBorder;
+	}
+
+	
+}
